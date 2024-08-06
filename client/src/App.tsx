ac04de4806f8808bc71584da02a01a99e4ee73a4
Jabirrr <jabirmhd6786@gmail.com>
2024-08-06 09:26:27 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black text-white font-bold text-2xl'>
+        Its begginning....
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ server/src/app.ts
@@ -0,0 +1,53 @@
+import express, { Request , Response , NextFunction } from 'express';
+import dotenv from 'dotenv'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+// import router from './routes'
+// import { errorHandler } from './utils/errorHandler';
+
+//initialaizing the Env Configurations required for the project
+dotenv.config()
+
+// connecting database
+// require('./mongoose/config')
+
+const app = express();
+
+const port = process.env.PORT || 3005
+
+
+//Helper middlewares
+app.use( express.json() );
+app.use( cookieParser() );
+
+
+//Adding Cors Security
+app.use(
+  cors({
+    origin: process.env.ACCEPTED_ORGINS ,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+//Routes
+app.use('/api', ( req , res ) => {
+  res.send('its success')
+} );
+
+
+// 404 Not Found middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    status: 'error',
+    message: 'Not Found'
+  });
+});
+
+// Use error handling middleware
+// app.use(errorHandler);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
